874573ae7cfa2c9389149518796c3a5ab97c88e2
BabyUnicorn90 <parancho@naver.com>
2020-12-03 10:22:03 +0900

    1203 bitflix v1.0

--- src/routes/Search/SearchContainer.js
+++ src/routes/Search/SearchContainer.js
@@ -1,12 +1,87 @@
 import React from "react";
+import { moviesApi, tvApi } from "../../api";
 import SearchPresenter from "./SearchPresenter";
 
 export default class extends React.Component {
+  state = {
+    movieResults: null,
+    tvResults: null,
+    searchTerm: "",
+    error: null,
+    loading: false,
+  };
+
+  // 만들어야하는 기능(함수):
+  // 1. submit: 서버에 보낸다. 2. update 3. search: process 를 진행한다.
+  // 1. submit:
+  handleSubmit = (event) => {
+    // preventDefault: SearchPresenter의 <Form>으로부터 submit된 작업(event)을 취소
+    // event에서 하고자하는일이 많을 때 customizing을 위해 사용. 
+    // ex. 비밀번호/id 중복검사 
+    event.preventDefault();
+
+    const { searchTerm } = this.state;
+
+    if (searchTerm !== "") {
+      this.searchByTerm;
+    }
+  };
+
+  // update: onChange에서 일어날 이벤트
+  updateTerm = (event) => {
+    const {
+      target: { value: searchTerm },
+    } = event;
+    this.setState({ searchTerm });
+  };
+
+  // search: 검색어를 넣은 API 호출
+  // event가 일어났을 때 비동기로 호출 --async함수!
+  searchByTerm = async () => {
+    // 1. 검색어 가져오기
+    const { searchTerm } = this.state;
+
+    // 2. loading 설정하기
+    this.setState({ loading: true });
+
+    // 3. 실제 api 호출하기
+    // 실제 data의 응답형태:
+    // { data: {results: [{}, {}, {}, ...]}}
+    try {
+      // movies(tv)Api.search -> movie(tv)Results에 담기
+      const {
+        data: { results: movieResults },
+      } = await moviesApi.search(searchTerm);
+      const {
+        data: { results: tvResults },
+      } = await tvApi.search(searchTerm);
+
+      this.setState({ movieResults, tvResults });
+    } catch (error) {
+      this.setState({ error: "결과를 찾을 수 없습니다." });
+    } finally {
+      this.setState({
+        loading: false,
+      });
+    }
+  };
+
   constructor(props) {
     super(props);
   }
 
   render() {
-    return <SearchPresenter />;
+    const { movieResults, tvResults, searchTerm, error, loading } = this.state;
+    return (
+      <SearchPresenter
+        movieResults={movieResults}
+        tvResults={tvResults}
+        searchTerm={searchTerm}
+        error={error}
+        loading={loading}
+        handleSubmit={this.handleSubmit}
+        updateTerm={this.updateTerm}
+      />
+    );
   }
 }
--- src/routes/Search/SearchPresenter.js
+++ src/routes/Search/SearchPresenter.js
@@ -1,11 +1,99 @@
-import React from 'react';
+import React from "react";
+import styled from "styled-components";
+import { Helmet } from "react-helmet";
 
-const SearchPresenter = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+import Loader from "../../components/Loader";
+import Message from "../../components/Message";
+import Poster from "../../components/Poster";
+import Section from "../../components/Section";
+
+const Container = styled.div`
+  padding: 20px;
+`;
+
+const Form = styled.form`
+  margin-bottom: 50px;
+  width: 100px;
+`;
+
+const Input = styled.input`
+    all: unset;
+    font-size = 28px;
+    width:100%;
+`;
+
+const SearchPresenter = ({
+  movieResults,
+  tvResults,
+  searchTerm,
+  error,
+  loading,
+  handleSubmit,
+  updateTerm,
+}) => {
+  return (
+    <Container>
+      <Helmet>
+        <title>Search | Bitflix</title>
+      </Helmet>
+      <Form onSubmit={handleSubmit}>
+        <Input
+          placeholder="Search for moves or tv shows"
+          value={searchTerm}
+          onChange={updateTerm}
+        />
+      </Form>
+
+      {loading ? (
+        <Loader />
+      ) : (
+        <>
+          {movieResults && movieResults.length > 0 && (
+            <Section title="Movie Results">
+              {movieResults.map((movie) => (
+                <Poster
+                  key={movie.id}
+                  id={movie.id}
+                  title={movie.original_title}
+                  imageUrl={movie.poster_path}
+                  rating={movie.vote_average}
+                  isMovie={true}
+                  year={
+                    movie.release_date && movie.release_date.substring(0, 4)
+                  }
+                ></Poster>
+              ))}
+            </Section>
+          )}
+
+          {tvResults && tvResults.length > 0 && (
+            <Section title="Tv shows Results">
+              {tvResults.map((show) => {
+                <Poster
+                  key={show.id}
+                  id={show.id}
+                  title={show.original_name}
+                  imageUrl={show.poster_path}
+                  rating={show.vote_average}
+                  year={
+                    show.first_air_date && show.first_air_date.substring(0, 4)
+                  }
+                  isMovie={false}
+                />;
+              })}
+            </Section>
+          )}
+          {error && <Message color={"e74c3c"} text={error} />}
+          {tvResults &&
+            movieResults &&
+            tvResults.length === 0 &&
+            movieResults.length === 0 && (
+              <Message text="no results" color={"#95a5a6"} />
+            )}
+        </>
+      )}
+    </Container>
+  );
 };
 
 export default SearchPresenter;
